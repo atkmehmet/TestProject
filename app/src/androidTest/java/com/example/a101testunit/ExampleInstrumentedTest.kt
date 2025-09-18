146a9789022f5da7968dfe2407fccf43e4a8c009
atkmehmet <durmazmehmet47@gmail.com>
2025-09-18 23:20:07 +0300

    gradle error ıs solved

--- app/src/androidTest/java/com/example/a101testunit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/a101testunit/ExampleInstrumentedTest.kt
@@ -2,11 +2,15 @@ package com.example.a101testunit
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.a101testunit.Design.UserViewModel
+import com.example.a101testunit.data.UserRepository
+import com.example.a101testunit.domain.GetUserNameUseCase
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,4 +25,28 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.a101testunit", appContext.packageName)
     }
+}
+
+
+class FakeUserRepository(): UserRepository {
+    override fun getUserName(): String = "Mehmet"
+
+}
+
+class UserViewModelTest{
+    private lateinit var viewModel: UserViewModel
+
+    @Before
+    fun setUp(){
+        val repo = FakeUserRepository()
+        val usecase = GetUserNameUseCase(repo)
+        viewModel = UserViewModel(usecase)
+    }
+
+    @Test
+    fun `getName returns Mehmet`() {
+        val result = viewModel.getName()
+        assertEquals("Mehmet", result)
+    }
+
 }
